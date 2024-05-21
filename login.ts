c8210a3fd23fe105a8cc6e39ecdba7a7a16df9ca
snehaSDET <sneha.singh@ibexlabs.com>
2024-05-21 14:30:23 +0530

    use of local & dev dependencies

--- login.ts
+++ login.ts
@@ -1,1 +1,19 @@
-// Merge Conflict demo
+import express from 'express';
+
+const app =express();
+
+app.get('/', (req, res) => {
+    console.log("Starting the app");
+    res.send("Welcome to the login page!")
+})
+
+app.get('/login', (req, res) => {
+    const username = req.query.username;
+    const password = req.query.password;
+    console.log("Starting the app");
+    res.json({message: `Welcome to the login page!  ${username}`})
+})
+
+
+
+app.listen(3000);

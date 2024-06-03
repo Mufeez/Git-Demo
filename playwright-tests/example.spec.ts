be9dbd90abdf16ff9621c8d4677c968536e8d806
snehaSDET <sneha.singh@ibexlabs.com>
2024-06-03 17:52:30 +0530

    misc changes

---
+++ playwright-tests/example.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+import { demoApiCall } from './scripts/command'
+
+test('make demo api call and assert the response', async ({}) => {
+  const url = 'https://jsonplaceholder.typicode.com/posts/1'
+
+  const response = await demoApiCall(url);
+  console.log(JSON.stringify(response.data, null, 2));
+  expect(response).toBeTruthy;
+  expect(response.status).toBeTruthy;
+  expect(response.status).toEqual(200);
+  
+});
+
